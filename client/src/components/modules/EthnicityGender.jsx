204652201354d9eb09c3944e977fd7d5b68e8600
Rahsun <rahsunk@mit.edu>
2025-05-11 13:45:21 -0400

    Increased size of ethnicity, gender graph

--- client/src/components/modules/EthnicityGender.jsx
+++ client/src/components/modules/EthnicityGender.jsx
@@ -4,16 +4,26 @@ import { Link, useNavigate } from "react-router-dom";
 // import { UserContext } from "../context/UserContext";
 import { get, post } from "../../utilities";
 import { getGenderCountByEthnicity } from "../modules/data.js";
-import { BarChart, CartesianGrid, XAxis, YAxis, Tooltip, Legend, Bar } from "recharts";
+import {
+  BarChart,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Legend,
+  Bar,
+} from "recharts";
 
 const EthnicityGender = (props) => {
-  const [genderStats, setGenderStats] = useState(getGenderCountByEthnicity(props.people));
+  const [genderStats, setGenderStats] = useState(
+    getGenderCountByEthnicity(props.people)
+  );
 
   return (
     <>
       <h2>Clients by Race and Gender</h2>
       {/* {genderStatList} */}
-      <BarChart width={730} height={250} data={genderStats}>
+      <BarChart width={1200} height={250} data={genderStats}>
         <CartesianGrid strokeDasharray="3 3" />
         <XAxis dataKey="ethnicity" />
         <YAxis />
